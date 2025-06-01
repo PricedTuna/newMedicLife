3fde6500e538c24f0f3d8f86946791a9516c3dd6
EdgarVega359 <edgar.vega@tecnologiasintech.com>
2025-06-01 03:12:16 -0700

    Update main layout styles and template metadata
    Revised the `main` element styles in CSS to adjust margin settings and improve layout consistency. Updated the compiled template metadata to reflect recent changes without altering functionality.

--- views/user/list/templates_c/618f4818f17cbb451db906d5a4602b3d410e1a26_0.file_list-users.view.tpl.php
+++ views/user/list/templates_c/618f4818f17cbb451db906d5a4602b3d410e1a26_0.file_list-users.view.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 5.4.5, created on 2025-06-01 09:48:13
+/* Smarty version 5.4.5, created on 2025-06-01 10:07:46
   from 'file:list-users.view.tpl' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.4.5',
-  'unifunc' => 'content_683c21dd5e1fd1_16180539',
+  'unifunc' => 'content_683c26721da019_02610010',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '618f4818f17cbb451db906d5a4602b3d410e1a26' => 
     array (
       0 => 'list-users.view.tpl',
-      1 => 1748770419,
+      1 => 1748772457,
       2 => 'file',
     ),
   ),
@@ -21,7 +21,7 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
     'file:../../components/sidebar.tpl' => 1,
   ),
 ))) {
-function content_683c21dd5e1fd1_16180539 (\Smarty\Template $_smarty_tpl) {
+function content_683c26721da019_02610010 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = '/var/www/html/views/user/list';
 ?><!DOCTYPE html>
 <html lang="es">
